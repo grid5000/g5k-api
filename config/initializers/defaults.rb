7d93fb0b13ecc0dd99d12e300e1f582005085a35
Cyril Rohr <cyril.rohr@inria.fr>
2011-10-13 13:55:34 +0200

    Adding RackDebugger to log request head and body.

--- config/initializers/defaults.rb
+++ config/initializers/defaults.rb
@@ -1,2 +1,4 @@
 # http://apidock.com/rails/ActiveRecord/Serialization/to_json
 ActiveRecord::Base.include_root_in_json = false
+
+Api::Application.config.middleware.use RackDebugger, Rails.logger
---
+++ lib/rack_debugger.rb
@@ -0,0 +1,18 @@
+class RackDebugger
+  def initialize(app, logger)
+    @app = app
+    @logger = logger
+  end
+  def call(env)
+    @logger.info ["  HEAD:", env['REQUEST_METHOD'], env['PATH_INFO'], "-", env.reject{|k,v|
+      k !~ /^HTTP\_/ || ["HTTP_X_FORWARDED_HOST", "HTTP_VIA", "HTTP_X_FORWARDED_SERVER", "HTTP_X_FORWARDED_FOR", "HTTP_AUTHORIZATION"].include?(k)
+    }.inspect].join(" ")
+
+    if env['rack.input']
+      @logger.info ["  BODY:", env['rack.input'].size > 10_000 ? "too big to display" : env['rack.input'].read.inspect].join(" ")
+      env['rack.input'].rewind
+    end
+
+    @app.call(env)
+  end
+end
