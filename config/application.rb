35cb7f6f26a05164ee3bff4dc9021c05ddaddc29
Cyril Rohr <cyril.rohr@inria.fr>
2011-10-24 17:15:30 +0200

    No longer storing config files in config/options/

--- config/application.rb
+++ config/application.rb
@@ -16,27 +16,11 @@ require 'em-http'
 require 'addressable/uri'
 require 'rack/fiber_pool'
 require 'rack/jsonp'
-require 'rack/fuzzy_accept'
 require 'rack/lint'
 
 module Api
 
   class Application < Rails::Application
-
-    DATABASE_CONFIG_PATHS = [
-      ENV['G5K_API_DATABASE_CONFIG'],
-      ENV['HOME'] ? "~/.g5k-api/database.yml" : nil,
-      "/etc/g5k-api/database.yml",
-      Rails.root.join("config/options/database.yml").to_path
-    ].compact
-
-    DEFAULTS_CONFIG_PATHS = [
-      ENV['G5K_API_DEFAULTS_CONFIG'],
-      ENV['HOME'] ? "~/.g5k-api/defaults.yml" : nil,
-      "/etc/g5k-api/defaults.yml",
-      Rails.root.join("config/options/defaults.yml").to_path
-    ].compact
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -61,27 +45,8 @@ module Api
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-
-    puts "Looking for database configuration file in #{DATABASE_CONFIG_PATHS.inspect}..."
-    paths.config.database = DATABASE_CONFIG_PATHS.find { |path|
-      fullpath = File.expand_path(path)
-      File.exist?(fullpath) && File.readable?(fullpath)
-    }
-    puts "=> Using database configuration file located at: #{paths.config.database.paths[0]}"
-    
-    
-    puts "Looking for defaults configuration file in #{Api::Application::DEFAULTS_CONFIG_PATHS.inspect}..."
-    config_file = Api::Application::DEFAULTS_CONFIG_PATHS.find { |path|
-      fullpath = File.expand_path(path)
-      File.exist?(fullpath) && File.readable?(fullpath)
-    }
-    if config_file.nil?
-      fail "=> Cannot find an existing and readable file in #{DEFAULTS_CONFIG_PATHS.inspect}"
-    else
-      puts "=> Using defaults configuration file located at: #{config_file}"
-    end
-
-    CONFIG = YAML.load_file(config_file)[Rails.env]
+    p Rails.root
+p [File.join(Rails.root, "config", "defaults.yml")]
+    CONFIG = YAML.load_file(File.join(Rails.root, "config", "defaults.yml"))[Rails.env]
   end
 end
