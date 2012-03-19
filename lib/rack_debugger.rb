a32ecbc2e4aff81f901825acbfc926366eff10b3
Cyril Rohr <cyril.rohr@inria.fr>
2012-03-19 13:32:53 +0100

    Revert "Do not output debugging info for pings originating from Squid proxy."
    This reverts commit 0476950537ef0b724d997610b44cf40aef7b9dd1.

--- lib/rack_debugger.rb
+++ lib/rack_debugger.rb
@@ -4,16 +4,13 @@ class RackDebugger
     @logger = logger
   end
   def call(env)
-    # Do not output debugging info for pings originating from Squid proxy.
-    unless env['HTTP_USER_AGENT'] =~ /^squid/i
-      @logger.info ["  HEAD:", env['REQUEST_METHOD'], env['PATH_INFO'], "-", env.reject{|k,v|
-        k !~ /^HTTP\_/ || ["HTTP_X_FORWARDED_HOST", "HTTP_VIA", "HTTP_X_FORWARDED_SERVER", "HTTP_X_FORWARDED_FOR", "HTTP_AUTHORIZATION"].include?(k)
-      }.inspect].join(" ")
+    @logger.info ["  HEAD:", env['REQUEST_METHOD'], env['PATH_INFO'], "-", env.reject{|k,v|
+      k !~ /^HTTP\_/ || ["HTTP_X_FORWARDED_HOST", "HTTP_VIA", "HTTP_X_FORWARDED_SERVER", "HTTP_X_FORWARDED_FOR", "HTTP_AUTHORIZATION"].include?(k)
+    }.inspect].join(" ")
 
-      if env['rack.input']
-        @logger.info ["  BODY:", env['rack.input'].read(10_000).inspect].join(" ")
-        env['rack.input'].rewind
-      end
+    if env['rack.input']
+      @logger.info ["  BODY:", env['rack.input'].read(10_000).inspect].join(" ")
+      env['rack.input'].rewind
     end
 
     @app.call(env)
