0476950537ef0b724d997610b44cf40aef7b9dd1
Cyril Rohr <cyril.rohr@inria.fr>
2012-03-19 11:46:01 +0100

    Do not output debugging info for pings originating from Squid proxy.

--- lib/rack_debugger.rb
+++ lib/rack_debugger.rb
@@ -4,13 +4,16 @@ class RackDebugger
     @logger = logger
   end
   def call(env)
-    @logger.info ["  HEAD:", env['REQUEST_METHOD'], env['PATH_INFO'], "-", env.reject{|k,v|
-      k !~ /^HTTP\_/ || ["HTTP_X_FORWARDED_HOST", "HTTP_VIA", "HTTP_X_FORWARDED_SERVER", "HTTP_X_FORWARDED_FOR", "HTTP_AUTHORIZATION"].include?(k)
-    }.inspect].join(" ")
+    # Do not output debugging info for pings originating from Squid proxy.
+    unless env['HTTP_USER_AGENT'] =~ /^squid/i
+      @logger.info ["  HEAD:", env['REQUEST_METHOD'], env['PATH_INFO'], "-", env.reject{|k,v|
+        k !~ /^HTTP\_/ || ["HTTP_X_FORWARDED_HOST", "HTTP_VIA", "HTTP_X_FORWARDED_SERVER", "HTTP_X_FORWARDED_FOR", "HTTP_AUTHORIZATION"].include?(k)
+      }.inspect].join(" ")
 
-    if env['rack.input']
-      @logger.info ["  BODY:", env['rack.input'].read(10_000).inspect].join(" ")
-      env['rack.input'].rewind
+      if env['rack.input']
+        @logger.info ["  BODY:", env['rack.input'].read(10_000).inspect].join(" ")
+        env['rack.input'].rewind
+      end
     end
 
     @app.call(env)
