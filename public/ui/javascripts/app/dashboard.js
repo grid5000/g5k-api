354825e1fb071abbffe140128f6224008dcacc42
Sebastien Badia <sebastien.badia@inria.fr>
2013-08-28 18:52:22 +0200

    [ui/js] Remove killer feature (twitter timeline on dashboard)

--- public/ui/javascripts/app/dashboard.js
+++ public/ui/javascripts/app/dashboard.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var widgets_to_display = [
     {id: 'status', refresh:300000, total: true}
   ]
-  Widget.display($.extend({id: 'twitter', refresh: 600000, limit: 6}, {container: '#news', api_base_uri: ".."}))
+  //Widget.display($.extend({id: 'twitter', refresh: 600000, limit: 6}, {container: '#news', api_base_uri: ".."}))
   $.each(widgets_to_display, function(i, widget_to_display) {
     var container_id = 'widget-'+widget_to_display.id;
     $("#widgets").append('<li id="'+container_id+'" class="widget"></li>');
