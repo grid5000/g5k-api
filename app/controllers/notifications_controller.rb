0e4cf23d6f53a37453b29a796f36cf511d3d852b
Clement Parisot <clement.parisot@inria.fr>
2014-12-16 09:41:50 +0100

    github

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,21 @@
+# Copyright (c) 2009-2011 Cyril Rohr, INRIA Rennes - Bretagne Atlantique
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 #  notifications_controller.rb
 #  g5k-api
 #
 #  Created by Cyril Rohr on 2011-10-10.
-#  Copyright 2011 Cyril Rohr. All rights reserved.
-#
 
 class NotificationsController < ApplicationController
 
