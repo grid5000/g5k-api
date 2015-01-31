5d4bdca53bce6e04a38004de50874a2d7f9068eb
David Margery <david.margery@inria.fr>
2015-01-31 14:42:46 +0100

    Move database creation to the development specific recipes

--- puppet/development.pp
+++ puppet/development.pp
@@ -7,6 +7,28 @@ class development {
   include ruby
   include git
   include dpkg::dev
+
+  exec{ "create development database":
+    user => root, group => root,
+    require => Exec["allow mysql connections from all"],
+    command =>  "/usr/bin/mysql -e \"create database g5kapi_development CHARACTER SET utf8 COLLATE utf8_general_ci;\"",
+    creates => "/var/lib/mysql/g5kapi_development"
+  }
+
+  exec{ "create test database":
+    user => root, group => root,
+    require => Exec["allow mysql connections from all"],
+    command =>  "/usr/bin/mysql -e \"create database g5kapi_test CHARACTER SET utf8 COLLATE utf8_general_ci;\"",
+    creates => "/var/lib/mysql/g5kapi_test"
+  }
+
+  exec{ "create oar2 database":
+    user => root, group => root,
+    require => Exec["allow mysql connections from all"],
+    command =>  "/usr/bin/mysql -e \"create database oar2 CHARACTER SET utf8 COLLATE utf8_general_ci;\"",
+    creates => "/var/lib/mysql/oar2"
+  }
+
 }
 
 stage { "init": before  => Stage["main"] }
