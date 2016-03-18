1eca7dc3de38c24d9eba13df5c89bccaae49156d
David Margery <david.margery@inria.fr>
2016-03-18 17:54:51 +0100

    [provisioning] add some more precise dependencies

--- puppet/modules/ruby/manifests/init.pp
+++ puppet/modules/ruby/manifests/init.pp
@@ -4,18 +4,24 @@ class ruby {
     ensure => latest
   }
   
+  # pacakges required to build native extensions
+  package {[
+    'libxml2-dev', #nokogiri
+    'libxslt1-dev' #nokogiri
+  ]: ensure => installed}
+
   exec { "install rake":
     user => root, group => root,
     command => "/usr/bin/gem1.9.3 install --no-ri --no-rdoc rake",
     require => Package["ruby1.9.3"],
-    creates => "/var/lib/gems/1.9.3/bin/rake"
+    creates => "/usr/local/bin/rake"
   }
 
   exec { "install bundler":
     user => root, group => root,
-    command => "/usr/bin/gem1.9.3 install --no-ri --no-rdoc bundler",
+    command => "/usr/bin/gem1.9.3 install --no-ri --no-rdoc bundler --version 1.7.6",
     require => Package["ruby1.9.3"],
-    creates => "/var/lib/gems/1.9.3/bin/bundle"
+    creates => "/usr/local/bin/bundle"
   }
 
 }
