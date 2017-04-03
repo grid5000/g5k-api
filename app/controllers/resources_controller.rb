73f730d3c3cdb0925957e7764996a5e69c600401
David Margery <david.margery@inria.fr>
2017-04-03 14:41:51 +0200

    move code to extract an object from the git repo according to params into a specific function

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -35,50 +35,21 @@ class ResourcesController < ApplicationController
     # abasu : Added code for getting 'queues' element in hash params - 11.12.2015
     # abasu : In request to feature bug ref. #6363
     # abasu : params_queues is the array with 'queues' values passed in 'params'
-    params_queues = []
     if params[:queues].nil? # no filter, so assign everything except "production"
-       params_queues = ["admin","default"] 
+       params[:queues] = ["admin","default"] 
     # As of 11.12.2015 the queues accepted are:
     # "all" or any combination of "admin", "default", "production"
     else 
        if params[:queues] == "all" # for use by sys-admin
-          params_queues = ["admin","default","production"]
+          params[:queues] = ["admin","default","production"]
        else
-          params_queues = params[:queues].split(",")
+          params[:queues] = params[:queues].split(",")
        end # if params[:queues] == "all"
     end # if params[:queues].nil?
 
-    object = EM::Synchrony.sync repository.async_find(
-      path.gsub(/\/?platforms/,''),
-      :branch => branch,
-      :version => params[:version]
-    )
+    object=lookup_path(path,branch,params)
     
-    # abasu : case logic for treating different scenarios - 11.12.2015
-    case [params[:controller], params[:action]]
-
-    # 1. case of a single cluster
-    when ["clusters", "show"] 
-       # Add ["admin","default"] to 'queues' if nothing defined for that cluster
-       object['queues'] = ["admin","default"] if object['queues'].nil?
-       object = nil if (object['queues'] & params_queues).empty?
-
-    # 2. case of an array of clusters
-    when ["clusters", "index"] 
-       # First, add ["admin","default"] to 'queues' if nothing defined for that cluster
-       object['items'].each { |cluster| cluster['queues'] = ["admin","default"] if cluster['queues'].nil? }
-       # Then, filter out 'queues' that are not requested in params
-       object['items'].delete_if { |cluster| (cluster['queues'] & params_queues).empty? }
-=begin
-       # This last step: to maintain current behaviour showing no 'queues' if not defined
-       # Should be removed when 'queues' in all clusters are explicitly defined.
-       object['items'].each { |cluster| cluster.delete_if { |key, value| key == 'queues' && value == ["default"] } }
-=end
-       # Finally, set new 'total' to clusters shortlisted
-       object['total'] = object['items'].length
-
-    end # case [params[:controller], params[:action]]
-
+    
     raise NotFound, "Cannot find resource #{path}" if object.nil?
     if object.has_key?('items')
       object['links'] = links_for_collection(object)
@@ -120,6 +91,40 @@ class ResourcesController < ApplicationController
     end
   end
 
+  def lookup_path(path, branch, params)
+    object = EM::Synchrony.sync repository.async_find(
+      path.gsub(/\/?platforms/,''),
+      :branch => branch,
+      :version => params[:version]
+    )
+    
+    # abasu : case logic for treating different scenarios - 11.12.2015
+    case [params[:controller], params[:action]]
+        
+    # 1. case of a single cluster
+    when ["clusters", "show"] 
+      # Add ["admin","default"] to 'queues' if nothing defined for that cluster
+      object['queues'] = ["admin","default"] if object['queues'].nil?
+      object = nil if (object['queues'] & params[:queues]).empty?
+      
+    # 2. case of an array of clusters
+    when ["clusters", "index"] 
+      # First, add ["admin","default"] to 'queues' if nothing defined for that cluster
+      object['items'].each { |cluster| cluster['queues'] = ["admin","default"] if cluster['queues'].nil? }
+      # Then, filter out 'queues' that are not requested in params
+      object['items'].delete_if { |cluster| (cluster['queues'] & params[:queues]).empty? }
+=begin
+         # This last step: to maintain current behaviour showing no 'queues' if not defined
+         # Should be removed when 'queues' in all clusters are explicitly defined.
+         object['items'].each { |cluster| cluster.delete_if { |key, value| key == 'queues' && value == ["default"] } }
+=end
+      # Finally, set new 'total' to clusters shortlisted
+      object['total'] = object['items'].length
+      
+    end # case [params[:controller], params[:action]]
+    object
+  end
+                  
   # Must be overwritten by descendants
   def collection_path
     raise NotImplemented
