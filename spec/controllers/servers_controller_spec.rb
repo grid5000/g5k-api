f6c842d659a0943d1a13c28e673ed78a1d87aaf5
David Margery <david.margery@inria.fr>
2017-09-14 20:51:57 +0200

    Migrate specs from deprecated .should syntax

--- spec/controllers/servers_controller_spec.rb
+++ spec/controllers/servers_controller_spec.rb
@@ -24,8 +24,8 @@ describe ServersController do
     it "should return ONLY cluster talc-data in nancy" do      
       get :show, :branch => 'master', :site_id => "nancy", :id => "talc-data", :format => :json
       assert_media_type(:json)
-      response.body.should == '{"alias":[],"kind":"physical","monitoring":{"metric":"power","wattmeter":"multiple"},"network_adapters":{"bmc":{"ip":"172.17.79.21"},"default":{"ip":"172.16.79.21"}},"sensors":{"network":{"available":true,"resolution":1},"power":{"available":true,"resolution":1,"via":{"pdu":[{"port":20,"uid":"grisou-pdu1"},{"port":20,"uid":"grisou-pdu2"}]}}},"serial":"92ZLL82","type":"server","uid":"talc-data","warranty":11.202,"version":"8a562420c9a659256eeaafcfd89dfa917b5fb4d0","links":[{"rel":"self","type":"application/vnd.grid5000.item+json","href":"/sites/nancy/servers/talc-data"},{"rel":"parent","type":"application/vnd.grid5000.item+json","href":"/sites/nancy"},{"rel":"version","type":"application/vnd.grid5000.item+json","href":"/sites/nancy/servers/talc-data/versions/8a562420c9a659256eeaafcfd89dfa917b5fb4d0"},{"rel":"versions","type":"application/vnd.grid5000.collection+json","href":"/sites/nancy/servers/talc-data/versions"}]}'
-      response.status.should == 200
+      expect(response.body).to eq '{"alias":[],"kind":"physical","monitoring":{"metric":"power","wattmeter":"multiple"},"network_adapters":{"bmc":{"ip":"172.17.79.21"},"default":{"ip":"172.16.79.21"}},"sensors":{"network":{"available":true,"resolution":1},"power":{"available":true,"resolution":1,"via":{"pdu":[{"port":20,"uid":"grisou-pdu1"},{"port":20,"uid":"grisou-pdu2"}]}}},"serial":"92ZLL82","type":"server","uid":"talc-data","warranty":11.202,"version":"8a562420c9a659256eeaafcfd89dfa917b5fb4d0","links":[{"rel":"self","type":"application/vnd.grid5000.item+json","href":"/sites/nancy/servers/talc-data"},{"rel":"parent","type":"application/vnd.grid5000.item+json","href":"/sites/nancy"},{"rel":"version","type":"application/vnd.grid5000.item+json","href":"/sites/nancy/servers/talc-data/versions/8a562420c9a659256eeaafcfd89dfa917b5fb4d0"},{"rel":"versions","type":"application/vnd.grid5000.collection+json","href":"/sites/nancy/servers/talc-data/versions"}]}'
+      expect(response.status).to eq 200
     end # it "should return ONLY cluster talc in nancy without any queues filter" 
 
   end # "GET /sites/{{site_id}}/servers/{{id}}/"
@@ -40,14 +40,14 @@ describe ServersController do
     it "should return 2 servers in site nancy and their exact names" do
       get :index, :branch => 'master', :site_id => "nancy", :format => :json
       assert_media_type(:json)
-      response.status.should == 200
-      json["total"].should == 2
+      expect(response.status).to eq 200
+      expect(json["total"]).to eq 2
 
       serverList = []
       json["items"].each do |server|
          serverList = [server["uid"]] | serverList
       end
-      (serverList - ["storage5k", "talc-data"]).should be_empty
+      expect(serverList - ["storage5k", "talc-data"]).to be_empty
 
     end # it "should return ALL servers in site nancy" 
 
--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -20,40 +20,40 @@ describe VersionsController do
   describe "GET {{resource}}/versions" do
     it "should get the list of versions" do
       get :index, :resource => "/sites", :format => :json
-      response.status.should == 200
-      json["total"].should == 10
-      json["offset"].should == 0
-      json["items"].length.should == 10
-      json["links"].map{|l| l["rel"]}.sort.should == ["parent", "self"]
-      json["items"][0].keys.sort.should == ["author", "date", "links", "message", "type", "uid"]
-      json["items"][0]["links"].map{|l| l["rel"]}.sort.should == ["parent", "self"]
+      expect(response.status).to eq 200
+      expect(json["total"]).to eq 10
+      expect(json["offset"]).to eq 0
+      expect(json["items"].length).to eq 10
+      expect(json["links"].map{|l| l["rel"]}.sort).to eq ["parent", "self"]
+      expect(json["items"][0].keys.sort).to eq ["author", "date", "links", "message", "type", "uid"]
+      expect(json["items"][0]["links"].map{|l| l["rel"]}.sort).to eq ["parent", "self"]
     end
     
     it "should return 404 if the resource does not exist" do
       get :index, :resource => "/does/not/exist", :format => :json
-      response.status.should == 404
+      expect(response.status).to eq 404
     end
   end # describe "GET {{resource}}/versions"
   
   describe "GET {{resource}}/versions/{{version_id}}" do
     it "should fail if the version does not exist" do
       get :show, :resource => "/", :id => "doesnotexist", :format => :json
-      response.status.should == 404
+      expect(response.status).to eq 404
       assert_vary_on :accept
-      response.body.should =~ %r{The requested version 'doesnotexist' does not exist or the resource '/' does not exist.}
+      expect(response.body).to match(%r{The requested version 'doesnotexist' does not exist or the resource '/' does not exist.})
     end
     
     it "should return the version" do
       version = "b00bd30bf69c322ffe9aca7a9f6e3be0f29e20f4"
       get :show, :resource => "/", :id => version, :format => :json
-      response.status.should == 200
+      expect(response.status).to eq 200
       assert_media_type(:json)
       assert_vary_on :accept
       assert_allow :get
       assert_expires_in 60.seconds, :public => true
-      json["uid"].should == version
-      json.keys.sort.should == ["author", "date", "links", "message", "type", "uid"]
-      json["author"].should == "Cyril Rohr"
+      expect(json["uid"]).to eq version
+      expect(json.keys.sort).to eq ["author", "date", "links", "message", "type", "uid"]
+      expect(json["author"]).to eq "Cyril Rohr"
     end
   end # describe "GET {{resource}}/versions/{{version_id}}"
   
