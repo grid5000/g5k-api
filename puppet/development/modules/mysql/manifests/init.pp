9cf618c24262f5a6fd81a7a8bb714ac379151503
Samir Noir <samir.noir@inria.fr>
2020-04-02 18:09:11 +0200

    upgrade the development environment to buster

--- puppet/development/modules/mysql/manifests/init.pp
+++ puppet/development/modules/mysql/manifests/init.pp
@@ -1,16 +1,22 @@
 class mysql {
-  package{"mysql-server":
+  package{"default-mysql-server":
     ensure => latest
   }
 
   service{ "mysql":
     ensure => running,
-    require => Package["mysql-server"]
+    require => Package["default-mysql-server"]
+  }
+
+  exec{ "set an empty root mysql password":
+    user => root, group => root,
+    command => "/usr/bin/mysql -e \"SET PASSWORD FOR 'root'@'localhost' = PASSWORD('');\"",
+    require => Service["mysql"]
   }
 
   exec{ "allow mysql connections from all":
     user => root, group => root,
-    command => "/usr/bin/mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';\"",
+    command => "/usr/bin/mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED via mysql_native_password;\"",
     require => Service["mysql"]
   }
 
@@ -20,10 +26,16 @@ class mysql {
     require => Service["mysql"]
   }
 
+  exec{ "flush privileges":
+    user => root, group => root,
+    command => "/usr/bin/mysql -e \"FLUSH PRIVILEGES;\"",
+    require => Service["mysql"]
+  }
+
   file{ "/etc/mysql/conf.d/custom.cnf":
     mode => "0644", owner => root, group => root,
     ensure => file,
-    require => Package["mysql-server"],
+    require => Package["default-mysql-server"],
     content => "[mysqld]\nbind-address		= 0.0.0.0\nport            = 3306\n",
     notify => Service["mysql"]
   }
