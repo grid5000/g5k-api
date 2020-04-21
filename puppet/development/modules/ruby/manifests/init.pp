a414ff2328d5790790ef453b756ad66bf65ce01c
Samir Noir <samir.noir@inria.fr>
2020-04-21 10:43:54 +0200

    change the way of installing bundler and rake inside the Vagrant VM and update the README

--- puppet/development/modules/ruby/manifests/init.pp
+++ puppet/development/modules/ruby/manifests/init.pp
@@ -1,26 +1,7 @@
 class ruby {
-  
-  package{"ruby":
+  $ruby_packages = ['ruby', 'ruby-dev', 'rake', 'bundler']
+
+  package{$ruby_packages:
     ensure => latest
   }
-  
-  package{"ruby-dev":
-    ensure => latest
-  }
-  
-  exec { "install rake":
-    user => root, group => root,
-    command => "/usr/bin/gem install --no-ri --no-rdoc rake -v 12.3.2",
-    require => Package["ruby"],
-    creates => "/usr/local/bin/rake"
-  }
-
-  exec { "install bundler":
-    user => root, group => root,
-    # bundler version must be less than 2.0.0 until running with ruby version < 2.3.0 as with debian jessie
-    command => "/usr/bin/gem install --no-ri --no-rdoc bundler --version '< 2.0.0'",
-    require => Package["ruby"],
-    creates => "/usr/local/bin/bundle"
-  }
-
 }
