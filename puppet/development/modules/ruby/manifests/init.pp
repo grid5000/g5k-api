40d2aa441c5490cc7a9cdf4f3bbc4c653bdd2d1f
Samir Noir <samir.noir@inria.fr>
2020-04-24 14:08:19 +0200

    Fix the way of installing bundler and rake (via gem) and remove jessie support from CI

--- puppet/development/modules/ruby/manifests/init.pp
+++ puppet/development/modules/ruby/manifests/init.pp
@@ -1,7 +1,21 @@
 class ruby {
-  $ruby_packages = ['ruby', 'ruby-dev', 'rake', 'bundler']
+  $ruby_packages = ['ruby', 'ruby-dev']
 
   package{$ruby_packages:
     ensure => latest
+  }
+
+  exec { "install rake":
+    user => root, group => root,
+    command => "/usr/bin/gem install --no-ri --no-rdoc rake",
+    require => Package["ruby"],
+    creates => "/usr/local/bin/rake"
+  }
+
+  exec { "install bundler":
+    user => root, group => root,
+    command => "/usr/bin/gem install --no-ri --no-rdoc bundler",
+    require => Package["ruby"],
+    creates => "/usr/local/bin/bundler"
   }
 }
