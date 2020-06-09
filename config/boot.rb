d624d7310409f190101d12c55005bb1786eee551
Samir Noir <samir.noir@inria.fr>
2020-06-09 15:33:13 +0200

    simplify boot.rb file

--- config/boot.rb
+++ config/boot.rb
@@ -14,33 +14,6 @@
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'rubygems'
-# Do not use the standard boot options
-# has we use a vendored bundler
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
-require 'yaml'
-
-# Attempts to use a vendored Bundler, if any
-vendored_gems = File.expand_path(
-  '../../vendor/bundle/ruby/2.5.0/gems', __FILE__
-)
-
-vendored_bundler = Dir["#{vendored_gems}/bundler-*/lib"].sort.last
-
-if !vendored_bundler.nil? && !$LOAD_PATH.include?(vendored_bundler)
-  $LOAD_PATH.unshift(vendored_bundler)
-end
-
-# Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
-rescue Bundler::GemNotFound => e
-  STDERR.puts e.message
-  STDERR.puts "Try running `bundle install`."
-  exit!
-end if File.exist?(gemfile)
