e3323ca1fd6541939c2b45e5234bd31e735e4890
Samir Noir <samir.noir@inria.fr>
2020-06-29 13:30:29 +0200

    Replace Grit by Rugged libgit2 binding for accessing git repository

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -14,92 +14,92 @@
 
 class VersionsController < ApplicationController
   MAX_AGE = 60.seconds
-  
-  
+
+
   def index
     vary_on :accept; allow :get
     versions = repository.versions_for(
-      resource_path, 
-      :branch => params[:branch], 
-      :offset => params[:offset], 
+      resource_path,
+      :branch => params[:branch],
+      :offset => params[:offset],
       :limit => params[:limit]
     )
-    
+
     raise NotFound, "#{resource_path} does not exist." if versions["total"] == 0
-    
+
     versions["items"].map!{|commit|
       metadata_for_commit(commit, resource_path)
     }
     versions["links"] = [
       {
-        "rel" => "self", 
-        "href" => uri_to("#{resource_path}/versions"), 
+        "rel" => "self",
+        "href" => uri_to("#{resource_path}/versions"),
         "type" => api_media_type(:g5kcollectionjson)
       },
       {
-        "rel" => "parent", 
-        "href" => uri_to("#{resource_path.split("/")[0..-2].join("/")}"), 
+        "rel" => "parent",
+        "href" => uri_to("#{resource_path.split("/")[0..-2].join("/")}"),
         "type" => api_media_type(:g5kitemjson)
       }
     ]
-    
+
     etag versions.hash
     expires_in MAX_AGE, :public => true
-    
+
     respond_to do |format|
       format.g5kcollectionjson { render :json => versions }
       format.json { render :json => versions }
     end
   end
-  
+
   def show
     vary_on :accept; allow :get
     version = params[:id]
-    
+
     versions = repository.versions_for(
-      resource_path, 
-      :branch => version, 
-      :offset => 0, 
+      resource_path,
+      :branch => version,
+      :offset => 0,
       :limit => 1
     )
     raise NotFound, "The requested version '#{version}' does not exist or the resource '#{resource_path}' does not exist." if versions["total"] == 0
     # etag compute_etag(commit.id, resource_uri, response['Content-Type'], options.release_hash)
-    
+
     output = metadata_for_commit(versions["items"][0], resource_path)
 
     etag versions.hash
     expires_in MAX_AGE, :public => true
-    
+
     respond_to do |format|
       format.g5kitemjson { render :json => output }
       format.json { render :json => output }
     end
   end
-  
+
   protected
   def resource_path
     @resource_path ||= params[:resource].gsub(/\/?platforms/, '')
   end
-  
+
   def metadata_for_commit(commit, resource_path)
-    { 
-      'uid' => commit.id,
-      'date' => commit.committed_date.httpdate,
+    {
+      'uid' => commit.oid,
+      'date' => commit.time.httpdate,
       'message' => commit.message,
-      'author' => commit.author.name,
+      'author' => commit.author[:name],
       'type' => 'version',
       'links' => [
         {
-          "rel" => "self", 
-          "href" => uri_to("#{resource_path}/versions/#{commit.id}"), 
+          "rel" => "self",
+          "href" => uri_to("#{resource_path}/versions/#{commit.oid}"),
           "type" => api_media_type(:g5kitemjson)
         },
         {
-          "rel" => "parent", 
-          "href" => uri_to(resource_path), 
+          "rel" => "parent",
+          "href" => uri_to(resource_path),
           "type" => api_media_type(:g5kitemjson)
         }
-      ] 
+      ]
     }
   end
 end
