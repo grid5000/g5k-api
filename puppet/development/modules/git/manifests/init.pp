28aa5b897657a4d7f6e6fa192c05072c5de5c9dc
Samir Noir <samir.noir@inria.fr>
2020-09-28 17:34:47 +0200

    Install missing packages to build Rugged Gem in vagrant provisioning

--- puppet/development/modules/git/manifests/init.pp
+++ puppet/development/modules/git/manifests/init.pp
@@ -2,4 +2,10 @@ class git {
   package{"git-core":
     ensure => latest
   }
+
+  # To build Rugged Gem
+  $rugged_build_deps = ['cmake', 'pkg-config']
+  package{$rugged_build_deps:
+    ensure => latest
+  }
 }
