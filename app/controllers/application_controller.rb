grid5000/g5k-api

commit 18511e935162b73554748931b571d62249c7c3da

Change the way of handling the differents possible Content-Type
g5k-api returns two differents Content-Type, for a single item or for
a collection of items. Commit f9ae4214bfce5721e288220511726fd852c94ca7
introduced a regression by always returning "simple" JSON instead of the
g5k-api's specifics Content-Type.
Now, the correct Content-Type will be returned, or application/json if
asked.

Fixes: f9ae4214bfce5721e288220511726fd852c94ca7

app/controllers/application_controller.rb
@@ -17,12 +17,7 @@ require 'swagger'
 class ApplicationController < ActionController::Base
   include ApplicationHelper
 
-  # Enable forgery request protection only in API
-  # See: https://api.rubyonrails.org/classes/ActionController/RequestForgeryProtection.html
-  protect_from_forgery unless: -> { request.format.json? }
-
   before_action :lookup_credentials
-  before_action :set_default_format
 
   # additional classes introduced to handle all possible exceptions
   # as per status codes https://api.grid5000.fr/doc/stable/reference/spec.html
@@ -71,8 +66,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def set_default_format
-    request.format = :json
+  def render_result(content, render_opts = {})
+    object = render_opts.merge(json: content)
+    respond_to do |format|
+      if object[:json].is_a?(Hash) && object[:json].has_key?('items')
+        format.g5kcollectionjson { render object }
+      else
+        format.g5kitemjson { render object }
+      end
+      format.any do
+        render object.merge(content_type: 'application/json')
+      end
+    end
   end
 
   def lookup_credentials

app/controllers/clusters_controller.rb
@@ -67,10 +67,7 @@ class ClustersController < ResourcesController
     expected_rtypes.push('disk') if params[:disks] != 'no'
     result.merge!(OAR::Resource.status(clusters: params[:id], network_address: params[:network_address], job_details: params[:job_details], waiting: params[:waiting], types: expected_rtypes))
 
-    respond_to do |format|
-      format.g5kitemjson { render json: result }
-      format.json { render json: result }
-    end
+    render_result(result)
   end
 
   protected

app/controllers/deployments_controller.rb
@@ -107,10 +107,7 @@ class DeploymentsController < ApplicationController
       'links' => links_for_collection
     }
 
-    respond_to do |format|
-      format.g5kcollectionjson { render json: result }
-      format.json { render json: result }
-    end
+    render_result(result)
   end
 
   swagger_path "/sites/{siteId}/deployments/{deploymentId}" do
@@ -172,10 +169,7 @@ class DeploymentsController < ApplicationController
     item = find_item(params[:id])
     item.links = links_for_item(item)
 
-    respond_to do |format|
-      format.g5kitemjson { render json: item }
-      format.json { render json: item }
-    end
+    render_result(item)
   end
 
   # Delete a deployment. Client must be authenticated and must own the deployment.
@@ -251,10 +245,7 @@ class DeploymentsController < ApplicationController
       location: location_uri,
       status: 201
     }
-    respond_to do |format|
-      format.g5kitemjson { render render_opts.merge(json: dpl) }
-      format.json { render render_opts.merge(json: dpl) }
-    end
+    render_result(dpl, render_opts)
   end
 
   # If the deployment is in the "canceled", "error", or "terminated" state,

app/controllers/jobs_controller.rb
@@ -118,10 +118,7 @@ class JobsController < ApplicationController
       'links' => links_for_collection
     }
 
-    respond_to do |format|
-      format.g5kcollectionjson { render json: result }
-      format.json { render json: result }
-    end
+    render_result(result)
   end
 
   swagger_path "/sites/{siteId}/jobs/{jobId}" do
@@ -185,10 +182,7 @@ class JobsController < ApplicationController
     job.links = links_for_item(job)
 
     render_opts = { methods: %i[resources_by_type assigned_nodes] }
-    respond_to do |format|
-      format.g5kitemjson { render render_opts.merge(json: job) }
-      format.json { render render_opts.merge(json: job) }
-    end
+    render_result(job, render_opts)
   end
 
   # Delete a job. Client must be authenticated and must own the job.
@@ -276,10 +270,7 @@ class JobsController < ApplicationController
       location: location_uri,
       status: 201
     }
-    respond_to do |format|
-      format.g5kitemjson { render render_opts.merge(json: job) }
-      format.json { render render_opts.merge(json: job) }
-    end
+    render_result(job, render_opts)
   end
 
   protected

app/controllers/resources_controller.rb
@@ -124,14 +124,7 @@ class ResourcesController < ApplicationController
     end
     etag object.hash
 
-    respond_to do |format|
-      if object.has_key?('items')
-        format.g5kcollectionjson { render json: object }
-      else
-        format.g5kitemjson { render json: object }
-      end
-      format.json { render json: object }
-    end
+    render_result(object)
   end
 
   def enrich_params(params)

app/controllers/sites_controller.rb
@@ -132,10 +132,7 @@ class SitesController < ResourcesController
 
     result.merge!(OAR::Resource.status(clusters: valid_clusters, network_address: params[:network_address], job_details: params[:job_details], waiting: params[:waiting], types: expected_rtypes))
 
-    respond_to do |format|
-      format.g5kitemjson { render json: result }
-      format.json { render json: result }
-    end
+    render_result(result)
   end
 
   protected

app/controllers/versions_controller.rb
@@ -151,10 +151,7 @@ class VersionsController < ApplicationController
     etag versions.hash
     expires_in MAX_AGE, public: true
 
-    respond_to do |format|
-      format.g5kcollectionjson { render json: versions }
-      format.json { render json: versions }
-    end
+    render_result(versions)
   end
 
   def show
@@ -178,10 +175,7 @@ class VersionsController < ApplicationController
     etag versions.hash
     expires_in MAX_AGE, public: true
 
-    respond_to do |format|
-      format.g5kitemjson { render json: output }
-      format.json { render json: output }
-    end
+    render_result(output)
   end
 
   protected

app/controllers/vlans_controller.rb
@@ -57,10 +57,7 @@ class VlansController < ApplicationController
       item['links'] = links_for_item(item)
     end
 
-    respond_to do |format|
-      format.g5kcollectionjson { render json: result }
-      format.json { render json: result }
-    end
+    render_result(result)
   end
 
   swagger_path "/sites/{siteId}/vlans/{vlanId}" do
@@ -103,10 +100,7 @@ class VlansController < ApplicationController
     result['links'] = links_for_item(result)
     replace_kavlan_remote(result)
 
-    respond_to do |format|
-      format.g5kitemjson { render json: result }
-      format.json { render json: result }
-    end
+    render_result(result)
   end
 
   swagger_path "/sites/{siteId}/vlans/{vlanId}/dhcpd" do

commit 343dcd5d7fa1fb02c9a0b30653ab4c5c900e470c

Fix issue with CSRF token verification
Issue was introduced in 18511e935162b73554748931b571d62249c7c3da,
by removing `protect_from_forgery` from ApplicationController.
It is now fixed by setting it to the :null_session strategy, which is
the way to go for an API.

app/controllers/application_controller.rb
@@ -18,6 +18,9 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   before_action :lookup_credentials
+  # See: https://api.rubyonrails.org/classes/ActionController/RequestForgeryProtection.html
+  # Not needed for an API
+  protect_from_forgery with: :null_session
 
   # additional classes introduced to handle all possible exceptions
   # as per status codes https://api.grid5000.fr/doc/stable/reference/spec.html